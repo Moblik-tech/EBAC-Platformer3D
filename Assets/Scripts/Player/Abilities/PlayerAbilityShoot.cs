72604da461dc5387bf4fe9e65570495d6ae7b1b2
Moblik-tech <kaike.dias.gabani@gmail.com>
2026-07-28 21:14:52 -0300

    Ajuste de lógica para haver múltiplas armas para se usar

--- Assets/Scripts/Player/Abilities/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/Abilities/PlayerAbilityShoot.cs
@@ -1,27 +1,55 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
     public GunBase gunBase;
+    public GunBase secondGun;
+    public Transform gunPosition;
+
+    private GunBase _mainGun;
+    private GunBase _secondGun;
+    private GunBase _currentGun;
 
     protected override void Init()
     {
         base.Init();
 
+        CreateGun();
+
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
+
+        inputs.Gameplay.MainGun.performed += ctx => UseMainGun();
+        inputs.Gameplay.SecondGun.performed += ctx => UseSecondGun();
+    }
+
+    private void CreateGun()
+    {
+        _mainGun = Instantiate(gunBase, gunPosition);
+        _secondGun = Instantiate(secondGun, gunPosition);
+
+        _currentGun = _mainGun;
     }
 
     private void StartShoot()
     {
-        gunBase.StartShoot();
+        _currentGun.StartShoot();
         Debug.Log("Shoot started");
     }
 
     private void CancelShoot()
     {
-        gunBase.StopShoot();
+        _currentGun.StopShoot();
         Debug.Log("Shoot canceled");
+    }
+
+    void UseMainGun()
+    {
+        _currentGun = _mainGun;
+    }
+
+    void UseSecondGun()
+    {
+        _currentGun = _secondGun;
     }
 }
